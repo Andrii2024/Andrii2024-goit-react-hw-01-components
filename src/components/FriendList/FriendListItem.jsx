05696d51ab2bea0a5270b1d3d03a1da447d7ac60
Andrii2024 <krovlya2005@gmail.com>
2024-02-14 08:59:39 +0200

    14.02

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import s from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusName = isOnline ? s.online : s.offline;
   return (
     <li className={s.item}>
-      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
+      <span className={`status ${statusName}`}></span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
     </li>
